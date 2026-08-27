297ca0e38dba5d6a5db909c0b1ff560c256a1798
GampichXD <abrahambram295@gmail.com>
2026-08-27 13:11:20 +0700

    feat: force unregister service workers and clear cache to resolve PWA issues

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,19 @@ import './index.css'
 import App from './App.tsx'
 import { GlobalProvider } from './context/GlobalContext'
 
+// FORCE UNREGISTER ALL SERVICE WORKERS & CLEAR CACHE TO FIX STUCK PWA
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+  if (window.caches) {
+    caches.keys().then((names) => {
+      for (let name of names) caches.delete(name);
+    });
+  }
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GlobalProvider>
